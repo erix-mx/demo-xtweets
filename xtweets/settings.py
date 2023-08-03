cf91ebe3a18f65d48d1924d8f911f9fd952038ea
Erix <82125058+erix-mx@users.noreply.github.com>
2023-08-03 17:40:07 -0600

    Test with postgrSQL

--- xtweets/settings.py
+++ xtweets/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,16 @@ WSGI_APPLICATION = "xtweets.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     os.getenv('DB_NAME', 'demo'),
+        'USER':     os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST':     os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT':     os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
